65dc70b00f5a1a83446dbb2afca295f1c34ad3b6
Shahid khan <shahid.manit@gmail.com>
2016-01-10 17:26:47 -0600

    Adding Model

--- src/services/todo-service.ts
+++ src/services/todo-service.ts
@@ -1,6 +1,12 @@
 import {Injectable} from "angular2/core";
+import {TodoModel} from "../components/todo-model";
+
 
 @Injectable()
 export class TodoService{
-	todos = [];
+	todos:[TodoModel] = [
+		new TodoModel("eat"),
+		new TodoModel("sleep"),
+		new TodoModel("code")
+	];
 }
